15bb5a2440f26194b328c57b614f2da86075b56d
andrewmiles1 <andrew1024miles@gmail.com>
2022-04-22 17:00:10 -0700

    Added mutexes
    Added mutex to Item struct and created appropriate copy constructor/assignment operator.

--- HashTable.h
+++ HashTable.h
@@ -13,13 +13,18 @@
 enum CollisionHandleType {OPEN_ADDRESSING, CHAINING};
 enum HashType {MULTIPLICATION, DIVISION};
 
-//HashTable in working good state
 
 template<class T, class U>
 class HashTable{
     struct Item {
-        T myKey;
-        U myData;
+            Item();
+            Item(const Item& other);
+            ~Item();
+            Item& operator=(const Item& other);
+
+            T myKey;
+            U myData;
+            std::mutex my_mutex;
     };
 
     public:
@@ -43,10 +48,19 @@ class HashTable{
         HashType myHashType;
 };
 
+/**
+ * @brief Construct a new Hash Table object
+ * 
+ * @tparam T key type
+ * @tparam U value type
+ * @param func conversion function of custom key type to integer. Must return integer with single T parameter
+ * @param handleType how should hash collisions be handled. OPEN_ADDRESSING or CHAINING
+ * @param hash_type how should hash be calculated from key in int form. MULTIPLICATION or DIVISION
+ */
 template<class T, class U>
 HashTable<T, U>::HashTable(int (*func)(T), CollisionHandleType handleType, HashType hash_type){
     keyToInt = func;//save parsing function for hashing
-    //FIXME - add ability to set approximate table size. 
+    //FIXME - add ability to set approximate table size. (this requires overloading)
     //initialize our table to a prime size.
     //just set to a good default prime number 59
     table = new LinkedList<Item>[59]();
@@ -56,11 +70,25 @@ HashTable<T, U>::HashTable(int (*func)(T), CollisionHandleType handleType, HashT
     myCollisionHandling = handleType;
 }
 
+/**
+ * @brief Destroy the Hash Table object
+ * 
+ * @tparam T key type
+ * @tparam U value type
+ */
 template<class T, class U>
 HashTable<T, U>::~HashTable(){
     delete[] table;
 }
 
+/**
+ * @brief return hash value from int key
+ * 
+ * @tparam T key type
+ * @tparam U value type
+ * @param key integer to be converted
+ * @return int converted hash location
+ */
 template<class T, class U>
 int HashTable<T, U>::hash(T key){
     //convert passed key,
@@ -70,9 +98,15 @@ int HashTable<T, U>::hash(T key){
     return key_int % table_size;
 }
 
+/**
+ * @brief called when hash table needs to be resized. This can double as shrinking function or expansion function.
+ * 
+ * @tparam T key type
+ * @tparam U value type
+ * @param new_table_size size to shrink/expand to
+ */
 template<class T, class U>
 void HashTable<T, U>::resizeAndRehash(int new_table_size){
-
     LinkedList<Item>* new_table = new LinkedList<Item>[new_table_size];
     LinkedList<Item>* old_table = table;
     int old_size = table_size;
@@ -88,6 +122,14 @@ void HashTable<T, U>::resizeAndRehash(int new_table_size){
 
 }
 
+/**
+ * @brief Adds item to hash table
+ * 
+ * @tparam T key type
+ * @tparam U value type
+ * @param key item's key 
+ * @param data item's data
+ */
 template<class T, class U>
 void HashTable<T, U>::addItem(T key, U data){
     int key_index = hash(key);
@@ -99,6 +141,13 @@ void HashTable<T, U>::addItem(T key, U data){
     size++;
 }
 
+/**
+ * @brief Removes item from HashTable. if Item doesn't exist in table, nothing happens.
+ * 
+ * @tparam T key type
+ * @tparam U value type
+ * @param key item to be deleted's key
+ */
 template<class T, class U>
 void HashTable<T, U>::removeItem(T key){
     int key_index = hash(key);
@@ -106,24 +155,48 @@ void HashTable<T, U>::removeItem(T key){
     for(Item it : table[key_index]){
         if(it.myKey == key){
             table[key_index].remove(it);//remove it haha
+            size--;
         }
     }
-    size--;
+    
 }
 
+/**
+ * @brief empties list of all items. sets table to default size 59
+ * 
+ * @tparam T key type
+ * @tparam U value type
+ */
 template<class T, class U>
 void HashTable<T, U>::clear(){
+    //TODO resize table to 59 or a default size
     //clears list without resizing.
     for(int i = 0; i < table_size; i++){
         table[i].clear();
     }
 }
 
+/**
+ * @brief returns number of items in the hash table
+ * 
+ * @tparam T key type
+ * @tparam U value type
+ * @return unsigned int number of items in table 
+ */
 template<class T, class U>
 unsigned int HashTable<T, U>::getSize(){
     return size;
 }
 
+/**
+ * @brief Displays through cout the stored size count, a manual size count,
+ *        max collisions in one cell, minimum collisions in one cell
+ *       (not counting cells with 0 items.), and optionally every item in list.
+ * 
+ * @tparam T key type
+ * @tparam U value type
+ * @param display_full_list instructs function to display full list
+ */
 template<class T, class U>
 void HashTable<T, U>::displayDiagnostic(bool display_full_list){
     int count = 0;
@@ -156,6 +229,15 @@ void HashTable<T, U>::displayDiagnostic(bool display_full_list){
     }
 }
 
+/**
+ * @brief searches to see if key exists in table
+ * 
+ * @tparam T key type
+ * @tparam U value type
+ * @param find_key key to be found
+ * @return true key exists in table
+ * @return false key does not exist in table
+ */
 template<class T, class U>
 bool HashTable<T, U>::search(T find_key){
     int key_index = hash(find_key);
@@ -166,4 +248,49 @@ bool HashTable<T, U>::search(T find_key){
         }
     }
     return false;
+}
+
+//******************* HASHTABLE::ITEM IMPLEMENTATION *************************************
+template<class T, class U>
+HashTable<T, U>::Item::Item(){
+    //I'm not convinced I need a constructor destructor in this case
+}
+
+template<class T, class U>
+HashTable<T, U>::Item::~Item(){
+    //maybe something has to happen here with mutex?
+}
+
+/**
+ * @brief Construct a new Item from passed other item
+ * 
+ * @tparam T key type
+ * @tparam U value type
+ * @param other passed object to copy
+ */
+template<class T, class U>
+HashTable<T, U>::Item::Item(const Item& other){
+    std::unique_lock other_lock(other.my_mutex);
+    std::unique_lock this_lock(my_mutex);
+
+    myData = other.myData;
+    myKey = other.myKey;
+}
+
+/**
+ * @brief copy details of other item to mine
+ * 
+ * @tparam T key type
+ * @tparam U value type
+ * @param other item to copy from
+ * @return HashTable<T, U>::Item& freshly copied to item
+ */
+template<class T, class U>
+HashTable<T, U>::Item& HashTable<T, U>::Item::operator=(const Item& other){
+    std::unique_lock other_lock(other.my_mutex);
+    std::unique_lock this_lock(my_mutex);
+
+    myData = other.myData;
+    myKey = other.myKey;
+    return this;
 }
