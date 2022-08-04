550a2ecf93a03441c4e9c22400650d03728fc951
andrewmiles1 <andrew1024miles@gmail.com>
2022-08-03 21:24:22 -0700

    Updated example usage code

--- examples/ExampleUsage.cpp
+++ examples/ExampleUsage.cpp
@@ -1,31 +1,25 @@
 #include <iostream>
-#include "DataPoint.h"
-#include "LinkedList.h"
+#include "../include/HashTable.h"
 
-int old_main() {
-    //test linked list add last and size:
-    LinkedList<int> test_list;
-    test_list.addLast(5);
-    test_list.addLast(4);
-    test_list.addLast(8);
-    test_list.addLast(4);
-    test_list.addLast(5);
-    test_list.addLast(6);
-    std::cout << test_list.size() << std::endl;
-    test_list.clear();
-    std::cout << test_list.size() << std::endl;
+int main() {
+    andrewmiles2::HashTable<int, int> squares_table([](int num) {return num; });
 
-    std::cout << "Iterator testing:" << std::endl;
-    //test iterator:
-    test_list.addLast(1);
-    test_list.addLast(2);
-    test_list.addLast(3);
-    test_list.addLast(4);
-    test_list.addLast(5);
-    test_list.addLast(6);
-    LinkedList<int>::Iterator it = test_list.begin();
-    for(it = test_list.begin(); it != test_list.end(); ++it){
-        std::cout << *it << std::endl;
+    squares_table.update(1, 1);
+    squares_table.update(2, 4);
+    squares_table.update(3, 9);
+    squares_table.update(4, 16);
+    squares_table.update(5, 25);
+    squares_table.update(6, 36);
+    squares_table.update(7, 49);
+    squares_table.update(8, 64);
+    squares_table.update(9, 81);
+    squares_table.update(10, 100);
+    squares_table.update(11, 121);
+    squares_table.update(12, 144);
+
+
+    for (int i = 0; i < squares_table.getSize(); i++) {
+        std::cout << i << " squared is " << squares_table.get(i) << std::endl;
     }
 
     return 0;
